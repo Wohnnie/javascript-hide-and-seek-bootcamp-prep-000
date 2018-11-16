ad0fd0d3f034f47de13c30bc92f312c48e3c3b9c
Angie Quin <aquinones100@gmail.com>
2018-11-16 14:57:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+function getFirstSelector(div){
+  return document.querySelector(div)
+}
 
+function nestedTarget() {
+  document.getElementsbyClassName("ranked-list")
+  ranked-list.nested
+  return nested.target
+}
